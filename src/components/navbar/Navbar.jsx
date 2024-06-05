6870f6c6c77f13fff624c81a8b414986307b3f9e
Burakkturk <burak1845turk@icloud.com>
2024-06-05 19:42:08 +0300

    Navbar added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,22 +2,37 @@ import React from "react";
 import { HamburgerIcon, Logo, Menu, MenuLink, Nav } from "./Navbar.style";
 import { menuIcon } from "../../helper/iconData";
 import { useState } from "react";
-
+import { useAuthContext } from "../../context/AuthContext";
 
 const Navbar = () => {
-  const [toggle,setToggle] =useState(false);
+  const [toggle, setToggle] = useState(false);
+  const { user, setUser } = useAuthContext();
+
+  const logOut = () => {
+    setUser(false);
+    handleClose()
+  }
+  const handleClose = () => {
+    setToggle(false);
+  }
   return (
     <Nav justify="space-between" wrap="wrap">
-      <Logo>BT Library</Logo>
+      <Logo to="/">BT Library</Logo>
       <HamburgerIcon onClick={() => setToggle(!toggle)}>
         {menuIcon}
       </HamburgerIcon>
       <Menu showMenu={toggle}>
-        <MenuLink>Home</MenuLink>
-        <MenuLink>About</MenuLink>
-        <MenuLink>Login</MenuLink>
-        <MenuLink>Register</MenuLink>
-        <MenuLink>Logout</MenuLink>
+        <MenuLink to="/" onClick={handleClose}>Home</MenuLink>
+        <MenuLink to="/about" onClick={handleClose}>About</MenuLink>
+
+        {user ? (
+          <MenuLink to="/login" onClick={handleClose}>Logout</MenuLink>
+        ) : (
+          <>
+            <MenuLink to="/login" onClick={handleClose}>Login</MenuLink>
+            <MenuLink to="/register" onClick={handleClose}>Register</MenuLink>
+          </>
+        )}
       </Menu>
     </Nav>
   );
