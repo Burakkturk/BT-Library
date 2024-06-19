07ca271815cd3ecc6fc2bab5f83b493905dd513e
Burakkturk <burak1845turk@icloud.com>
2024-06-19 16:13:47 +0300

    detailstyle

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 const Detail = () => {
+  const {state} = useLocation()
+  console.log(state)
   return (
     <div>Detail</div>
   )
